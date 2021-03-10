4a27362cff7331b4faea06bb7c17aabf86f2d44f
Jarrel <jarrel.thomas@gmail.com>
2021-03-10 13:17:52 -0800

    ex5 additional tasks

--- ex5.py
+++ ex5.py
@@ -1,18 +1,24 @@
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie
-my_height = 74 # in inches
-my_weight = 180 #lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # in inches
+weight = 180 #lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
 
-print(f"Let's talk about {my_name}.")
-print(f"He's {my_height} inches tall.")
-print(f"He's {my_weight} pounds heavy.")
+height_metric = round(height * 0.3048)
+weight_metric = round(weight * 0.4535924) 
+
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"or {height_metric} meters tall.")
+print(f"He's {weight} pounds heavy.")
+print(f"or {weight_metric} kilograms heavy.")
 print("Actually that's not too heavy.")
-print(f"He's got {my_eyes} eyes and {my_hair} hair.")
-print(f"His teeth are usually {my_teeth} depending on the coffee.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
 
 # this line is tricky, try to get it exactly right
-total = my_age + my_height + my_weight
-print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
