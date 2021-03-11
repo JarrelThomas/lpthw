45dc218aa2cd1a31e2ef0a88d0bc8f1091baa783
Jarrel <jarrel.thomas@gmail.com>
2021-03-10 22:44:13 -0800

    Exercise 7

--- ex6.py
+++ ex6.py
@@ -1,22 +1,23 @@
-types_of_people = 10
-x = f"There are {types_of_people} types of people."
+types_of_people = 10 #assigns the variable of types of people with the value of 10
+x = f"There are {types_of_people} types of people." #assigns the variable of x formatting the string to insert the varible types of people into the value
 
-binary = "binary"
-do_not = "don't"
-y = f"Those who know {binary} and those who {do_not}."
 
-print(x)
-print(y)
+binary = "binary" #assigns the variable binary with the value of the same name
+do_not = "don't" #assigns the variable do not with value of don't
+y = f"Those who know {binary} and those who {do_not}." #assigns the y varibale with a formatted string to include the previous two varibles
 
-print(f"I said: {x}")
-print(f"I also said: '{y}'")
+print(x) #prints x variable
+print(y) #prints y variable
 
-hilarious = False
-joke_evaluation = "Isn't that joke so funny?! {}"
+print(f"I said: {x}") #prints formatted string with the x variable
+print(f"I also said: '{y}'") #prints formatted string with y variable
 
-print(joke_evaluation.format(hilarious))
+hilarious = False #assigns hilarious varible with False value
+joke_evaluation = "Isn't that joke so funny?! {}" # Assigns joke_eva..... variable with string and empty variable to insert one in later
 
-w = "This is the left side of..."
+print(joke_evaluation.format(hilarious)) #prints variable joke with the format syntax in order to insert hilarious variable
+
+w = "This is the left side of..." 
 e = "a string with a right side."
 
 print(w + e)
